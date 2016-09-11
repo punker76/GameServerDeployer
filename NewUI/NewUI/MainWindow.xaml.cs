0554cb0039cd446397aa69027c73626766a09a26
NetlyGroup <hey@netly.co>
2016-09-11 11:00:38 +0200

    Implemented most of the functionalities, still some issues tho

--- NewUI/NewUI/MainWindow.xaml.cs
+++ NewUI/NewUI/MainWindow.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
 namespace NewUI
 {
     /// <summary>
@@ -34,6 +25,7 @@ namespace NewUI
 
 
 
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -44,8 +36,83 @@ namespace NewUI
 
         private async void button_Click_1(object sender, RoutedEventArgs e)
         {
+
+            bool fdbShown = false;
+            MessageDialogResult msgRes = await this.ShowMessageAsync("EULA", "Do you accept the Minecraft EULA? \n(https://account.mojang.com/documents/minecraft_eula)", MessageDialogStyle.AffirmativeAndNegative);
+            if (msgRes == MessageDialogResult.Negative)
+            {
+                await this.ShowMessageAsync("EULA", "You need to accept the Minecraft EULA to host your own server.");
+                return;
+            }
+
             try
             {
+
+
+                var controller = await this.ShowProgressAsync("Initialization", "Starting process");
+                controller.SetProgress(0);
+                bool tcpOpen = false;
+                bool udpOpen = false;
+                using (TcpClient tcpClient = new TcpClient())
+                {
+                    try
+                    {
+                        tcpClient.Connect("localhost", 25565);
+                        tcpOpen = true;
+                        tcpClient.Close();
+                    }
+                    catch (Exception)
+                    {
+                        tcpOpen = false;
+                        tcpClient.Close();
+
+                    }
+                    using (UdpClient udpClient = new UdpClient())
+                        try
+                        {
+                            udpClient.Connect("localhost", 25565);
+                            udpOpen = true;
+                            udpClient.Close();
+                        }
+                        catch (Exception)
+                        {
+                            udpOpen = false;
+                            udpClient.Close();
+                        }
+                    await this.ShowMessageAsync("Choosing path", "Ok, so you're going to choose the path for your server", MessageDialogStyle.Affirmative);
+                    controller.SetTitle("Path");
+                    controller.SetMessage("Choosing the path");
+                    controller.SetProgress(0.1);
+
+                    FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+                    DialogResult result = fbd.ShowDialog();
+                    fdbShown = true;
+                    if (fdbShown == true)
+                    {
+                        controller.SetTitle("Ports info");
+                        controller.SetProgress(0.2);
+                        if (tcpOpen == false && udpOpen == false)
+                        {
+                            controller.SetMessage("Neither of the two ports (UDP & TCP 25565) are opened, you can always modify the port in the config or open them later.");
+                        }
+                        if (tcpOpen == false && udpOpen == true)
+                        {
+                            controller.SetMessage("The TCP port 25565 isn't opened, you can always modify the port in the config or open it later. The UDP port 25565 is opened.");
+                        }
+                        if (tcpOpen == true && udpOpen == false)
+                        {
+                            controller.SetMessage("The UDP port 25565 isn't opened, you can always modify the port in the config or open it later. The TCP port 25565 is opened.");
+                        }
+                        if (tcpOpen == true && udpOpen == true)
+                        {
+                            controller.SetMessage("All of the ports (UDP & TCP 25565) are opened! :)");
+                        }
+                        Thread.Sleep(500);
+
+
+                    }
+
                 if (this.serverType.SelectedItem.ToString() == "Spigot")
                 {
                     if (this.serverVersion.SelectedItem.ToString() == "1.8.8")
@@ -60,72 +127,47 @@ namespace NewUI
                                 return;
                             }
                             else
-                            {
-                                var controller = await this.ShowProgressAsync("Initialization", "Starting process");
-                                controller.SetProgress(0);
-                                bool tcpOpen = false;
-                                bool udpOpen = false;
-                                using (TcpClient tcpClient = new TcpClient())
                                 {
-                                    try
-                                    {
+                                    controller.SetTitle("Downloading files");
+                                    controller.SetMessage("Downloading server files: eula.txt");
+                                    WebClient webClient = new WebClient();
+                                    webClient.DownloadFileAsync(new Uri("https://box.netly.co/ServerDeployer/eula.txt"), fbd.SelectedPath.ToString() + "\\eula.txt");
+                                    WebClient webClient2 = new WebClient();
+                                    controller.SetProgress(0.4);
+                                    controller.SetMessage("Downloading server files: spigot.jar");
+                                    webClient2.DownloadFileAsync(new Uri("https://box.netly.co/ServerDeployer/Spigot/1.8.8/spigot.jar"), fbd.SelectedPath.ToString() + "\\spigot.jar");
+                                    controller.SetProgress(0.6);
+                                    WebClient webClient3 = new WebClient();
+                                    controller.SetMessage("Downloading server files: start.cmd");
+                                    webClient3.DownloadFileAsync(new Uri("https://box.netly.co/ServerDeployer/Spigot/Start/2G/start.cmd"), fbd.SelectedPath.ToString() + "\\start.cmd");
+                                    controller.SetProgress(0.8);
+                                    controller.SetTitle("Starting server");
+                                    controller.SetMessage("Starting the server for you :)");
+                                    controller.SetProgress(0.9);
+                                    webClient3.Dispose();
 
-                                        
-                                        tcpClient.Connect("localhost", 25565);
-                                        tcpOpen = true;
-                                        tcpClient.Close();
+                                    Process.Start(fbd.SelectedPath + "\\start.cmd");
+                                    controller.SetTitle("Deployment succesful!");
+                                    controller.SetMessage("Your server was succesfully deployed and started. If you want to stop it, enter 'stop' in the console, if you want to start it again, open the folder and launch start.cmd\nEnjoy! :D");
+                                    controller.SetProgress(1);
+                                    Thread.Sleep(300);
+                                    await controller.CloseAsync();
 
-                                        //MetroFramework.MetroMessageBox.Show(this, "The TCP port 25565 isn't opened. If you want to use another port, you can always modify it in the config. The server deployer will continue.", "TCP Port not opened", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                                    }
-                                    catch (Exception)
-                                    {
-                                        tcpOpen = false;
-                                        tcpClient.Close();
-                                        
-                                    }
-                                    using (UdpClient udpClient = new UdpClient())
-                                        try
-                                        {
 
-                                            udpClient.Ttl = 10;
-                                            udpClient.Connect("localhost", 25565);
-                                            udpOpen = true;
-                                            udpClient.Close();
 
-                                            
-
-                                        }
-                                        catch (Exception)
-                                        {
-                                            udpOpen = false;
-                                            udpClient.Close();
-                                            
-                                        }
-                                    controller.SetTitle("Ports info");
-                                    controller.SetProgress(0.1);
-                                    if (tcpOpen == false && udpOpen == false)
-                                    {
-                                        
-                                        controller.SetMessage("Neither of the two ports (UDP & TCP 25565) are opened, you can always modify the port in the config or open them later.");
-
-                                    }
-                                    if (tcpOpen == false && udpOpen == true)
-                                    {
-                                        controller.SetMessage("The TCP port 25565 isn't opened, you can always modify the port in the config or open it later.");
-                                    }
-                                    if (tcpOpen == true && udpOpen == false)
-                                    {
-                                        controller.SetMessage("The UDP port 25565 isn't opened, you can always modify the port in the config or open it later.");
-                                    }
-                                    if (tcpOpen == true && udpOpen == true)
-                                    {
-                                        controller.SetMessage("All of the ports (UDP & TCP 25565) are opened! :)");
-                                    }
                                 }
 
+                        }
 
 
-                            }
+
+
+
+
+
+
+
+
 
 
 
@@ -134,21 +176,15 @@ namespace NewUI
 
 
                         }
-
-
                     }
-
                 }
-
             }
+
+
             catch (System.NullReferenceException)
             {
                 await this.ShowMessageAsync("Error", "Please choose something!", MessageDialogStyle.Affirmative);
             }
-            //var controller = await this.ShowProgressAsync("wesh wesh", "wesh");
-            //controller.SetProgress(0);
-            //controller.SetMessage("Bonjour");
-            //controller.SetProgress(0.9);
             
         }
         static ulong GetTotalMemoryInBytes()
